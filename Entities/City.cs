1bc68d5506d091277e4692a8915329e40e605baa
Jendosek <bala01081977@gmail.com>
2024-12-13 23:08:57 +0200

    task3

---
+++ Entities/City.cs
@@ -0,0 +1,38 @@
+namespace _10._12._24.Entities;
+
+public class City
+{
+    public int CountryCount { get; set; }
+    
+    public static City operator +(City city1, City city2)
+    {
+        return new City
+        {
+            CountryCount = city1.CountryCount + city2.CountryCount
+        };
+    }
+    
+    public static City operator -(City city1, City city2)
+    {
+        return new City
+        {
+            CountryCount = city1.CountryCount - city2.CountryCount
+        };
+    }
+    
+    public static City operator ==(City city1, City city2)
+    {
+        return new City
+        {
+            CountryCount = city1.CountryCount == city2.CountryCount ? 1 : 0
+        };
+    }
+    
+    public static City operator !=(City city1, City city2)
+    {
+        return new City
+        {
+            CountryCount = city1.CountryCount != city2.CountryCount ? 1 : 0
+        };
+    }
+}
